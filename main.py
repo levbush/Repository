0924f9704edb675187cf2bf6ace59efbdf63e396
levbush <lev.o.bushuev@gmail.com>
2025-11-21 19:41:05 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from random import randint
+from PyQt6.QtGui import QColor, QPainter
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300, 300, 500, 500)
+        self.btn = QPushButton('Magical button', self)
+        self.btn.resize(100, 50)
+        self.btn.clicked.connect(self.draw)
+        self.do_draw = False
+
+    def draw(self):
+        self.do_draw = True
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setPen(QColor('yellow'))
+        r = randint(10, 100)
+        painter.drawEllipse(250, 250, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
